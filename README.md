3822fb3887b609adfb544ae401e738d4895abb41
Arjun Narayanan <arjunnarayanan@arjuns-mbp.home>
2023-10-27 11:26:10 +0100

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
-# ccc_chatbot
-A chatbot to query the Climate Change Committee's documents
+# PDFAssistant
+
+PDFAssistant is an interactive tool that allows users to ask questions based on the content of PDF documents. It makes use of various libraries such as Streamlit, PyPDF2, and langchain to facilitate the querying process of the PDFs.
+
+## Dependencies
+
+- `streamlit`: For creating the web application
+- `dotenv`: For loading environment variables
+- `PyPDF2`: For PDF text extraction
+- `requests`: For downloading PDFs from URLs
+- `langchain`: A chain of NLP tools for text processing and embedding
+
+## Code Structure
+
+### `download_pdf_in_memory(url, failed_urls)`
+
+- Downloads a PDF from a given URL and stores it in memory.
+- Appends failed URLs to the `failed_urls` list.
+
+### `get_pdf_text(pdf_reader)`
+
+- Extracts text content from the PDF pages using a `PdfReader` object.
+
+### `get_pdf_text_from_docs(pdf_docs)`
+
+- Processes uploaded PDF documents and extracts text.
+
+### `get_pdf_text_from_urls(pdf_urls, failed_urls)`
+
+- Processes PDFs from given URLs and extracts text.
+
+### `get_text_chunks(text)`
+
+- Breaks down the PDF text into manageable chunks. Useful for processing.
+
+### `get_vectorstore(text_chunks)`
+
+- Initializes OpenAIEmbeddings and creates a vector store for text chunks.
+
+### `get_conversation_chain(vectorstore)`
+
+- Sets up a conversation chain, utilizing a language model and a vector store.
+
+### `display_message(message, sender="user")`
+
+- Displays messages in the Streamlit app, differentiating between user and bot.
+
+### `handle_userinput(user_question)`
+
+- Handles user queries and displays the conversation in the Streamlit app.
+
+### `main()`
+
+- Main function that initializes the Streamlit app and orchestrates the execution of functionalities.
+
+## Usage
+
+1. **Running the Application:**
+    - Open a web browser and go to `http://localhost:8501/`.
+    - You should see the PDFAssistant interface ready for interaction.
+
+2. **Adding Core PDFs:**
+    - Open the file `core_pdfs.txt` from the project directory.
+    - Copy the links listed in the file.
+    - Paste these links into the "PDF URLs" box in the PDFAssistant interface.
+
+3. **Adding Additional PDFs:**
+    - Users can add more PDFs by pasting additional links into the "PDF URLs" box.
+    - Users can also upload PDF files directly through the user interface.
+   
+4. **Processing PDFs:**
+    - After adding the PDFs, click the "Process" button. PDFAssistant will extract and process text from the added PDFs.
+   
+5. **Querying PDFs:**
+    - Enter your questions in the query box to receive responses based on the content of the processed PDFs.
+
+
+## Error Handling
+
+- The application includes error handling mechanisms to manage failed downloads and PDF processing issues.
+
+## Installation and Setup
+
+1. **Clone the Repository:**
+    ```bash
+    git clone https://github.com/yourusername/PDFAssistant.git
+    cd PDFAssistant
+    ```
+   
+2. **Create a Virtual Environment:**
+    ```bash
+    python3 -m venv env
+    source env/bin/activate  # For Windows, use `env\Scripts\activate`
+    ```
+   
+3. **Install Dependencies:**
+    ```bash
+    pip install -r requirements.txt
+    ```
+    Ensure the `requirements.txt` file includes the following:
+    ```
+    streamlit
+    python-dotenv
+    PyPDF2
+    requests
+    langchain
+    ```
+   
+4. **Set Up Environment Variables:**
+   - Create a `.env` file in the project directory.
+   - Add necessary environment variables, such as API keys, if needed.
+   
+5. **Run the Application:**
+    ```bash
+    streamlit run your_app_script.py
+    ```
+    The application should now be running at `http://localhost:8501/`.
+
+
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from io import BytesIO
+import requests
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -8,32 +10,81 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader=PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text +=page.extract_text()
+
+# Downloading PDFs from specified URLs
+def download_pdf_in_memory(url, failed_urls):
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return BytesIO(response.content)
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to download: {url} due to {e}")
+        failed_urls.append(url)
+        return None
+
+
+# Function to extract text from PDFs
+def get_pdf_text(pdf_reader):
+    text = ""
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
     return text
 
+
+# Extracting text from documents dropped from user's computer
+def get_pdf_text_from_docs(pdf_docs):
+    doc_text = ""
+    for pdf in pdf_docs:
+        try:
+            pdf_reader = PdfReader(pdf)
+            doc_text += get_pdf_text(pdf_reader)
+        except Exception as e:
+            st.error(f"An error occurred while processing the PDF: {e}")
+    return doc_text
+
+
+# Extracting text from documents for which links are provided
+def get_pdf_text_from_urls(pdf_urls, failed_urls):
+    text = ""
+    for url in pdf_urls:
+        pdf_in_memory = download_pdf_in_memory(url, failed_urls)
+        if pdf_in_memory:
+            try:
+                pdf_reader = PdfReader(pdf_in_memory)
+                text += get_pdf_text(pdf_reader)
+            except Exception as e:
+                st.error(f"An error occurred while processing the PDF from {url}: \
+                {e}")
+    return text
+
+
+# Breaking the final body of text into chunks
 def get_text_chunks(text):
-    text_splitter=CharacterTextSplitter(
+    text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200
+        chunk_size=800,
+        chunk_overlap=50
     )
-    chunks=text_splitter.split_text(text)
+    chunks = text_splitter.split_text(text)
     return chunks
 
+
+# Initialising OpenAIEmbeddings and creating vector store
 def get_vectorstore(text_chunks):
-    embeddings=OpenAIEmbeddings()
-    vectorstore=FAISS.from_texts(texts=text_chunks,embedding=embeddings)
-    # vectorstore.store_local("faiss_store")
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+
+# Initialising a conversation chain
 def get_conversation_chain(vectorstore):
-    llm=ChatOpenAI()
-    memory=ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+    llm=ChatOpenAI(temperature=0.9)
+    memory=ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
     conversation_chain=ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
@@ -41,60 +92,102 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
-# conversation object has all the configurations required
+
+# defining the display layout for user-chatbot messages
+def display_message(message, sender="user"):
+    if sender == "user":
+        st.text(f"👤 PDFAssistant: ")  # using unicode emoji
+        st.write(message)
+    else:
+        st.text(f"🤖 User: {message}")   # Using Unicode emoji
+
+
+# Function to handle user queries and produce output
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    # st.write(response.get("answer"))
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(message.content)
-        else:
-            st.write(message.content)
-
-
+    if st.session_state.conversation is None:
+        st.error("Please process the PDFs first before asking questions.")
+    else:
+        # the LLM is only triggered when there's a non-null query
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
+        for i, message in enumerate(st.session_state.chat_history):
+            sender = "bot" if i % 2 == 0 else "user"
+            display_message(message.content, sender=sender)
 
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=": books:")
-    llm=ChatOpenAI()
 
+    st.set_page_config(
+        page_title="PDFAssistant",
+        page_icon=":books:"
+    )
+
+    # setting the conversation key when it hasn't been initialised
     if "conversation" not in st.session_state:
         st.session_state.conversation=None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question=st.text_input("Ask a question about your documents:")
-    if user_question:
+    # title and subtitle display for streamlit interface
+    st.header("Question your PDFs :page_facing_up: :scroll: :computer: :mega:")
+    st.subheader("Welcome to PDFAssistant. Query any of your pdfs"
+                 " by first adding links or documents to the sidebar, "
+                 "clicking process and finally asking your questions")
+
+    # handling user queries
+    user_question = st.text_input("Query your documents with PDFAssistant:")
+    if user_question: # llm called only for valid non-null queries
         handle_userinput(user_question)
 
-
     with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs=st.file_uploader("Upload your PDFs here and click on 'Process'",accept_multiple_files=True)
+        st.subheader("PDF URLs")
+
+        # PDF link adding section
+        pdf_urls_area = "Enter PDF URLs (separate each URL by a newline):"
+        pdf_urls = st.text_area(pdf_urls_area).split("\n")
+        failed_urls = [] # Creating a list to hold failed URLs
+
+        # PDF document adding section
+        upload_msg = "OR/AND Upload your PDFs here and click on 'Process'"
+        pdf_docs = st.file_uploader(upload_msg, accept_multiple_files=True)
 
         if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text=get_pdf_text(pdf_docs)
+            try:
+                # display message when no pdfs or links are added by
+                # user
+                if (not pdf_docs) and (not any(url.strip() for url in pdf_urls)):
+                    st.error("Please add some PDFs or URLs to process.")
+                else:
+                    with st.spinner("Processing"):
+                        raw_text = ""
 
-                # get text chunks
-                text_chunks=get_text_chunks(raw_text)
-                st.write(text_chunks)
+                        # extracting text from pdf links
+                        if pdf_urls and any(url.strip() for url in pdf_urls):
+                            raw_text += get_pdf_text_from_urls(pdf_urls, failed_urls)
 
-                # create vector store
-                vectorstore=get_vectorstore(text_chunks)
+                            # displaying messages for failed pdf urls
+                            if failed_urls:
+                                msg = "Failed to download the following URLs:"
+                                st.sidebar.warning(msg)
+                                for url in failed_urls:
+                                    st.sidebar.text(url)
 
-                # create conversation chain
-                st.session_state.conversation= get_conversation_chain(vectorstore)
-                # conversation object --> takes history of conversation and returns next element
+                        # obtaining final string of downloaded pdf text
+                        raw_text += get_pdf_text_from_docs(pdf_docs)
 
+                        # dividing text into chunks
+                        text_chunks = get_text_chunks(raw_text)
 
+                        # creating vectorstore
+                        vectorstore = get_vectorstore(text_chunks)
+
+                        # initialising conversation chain
+                        conv_chain = get_conversation_chain(vectorstore)
+                        st.session_state.conversation = conv_chain
+
+            except Exception as e:
+                st.error(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
     main()
-
-
 
